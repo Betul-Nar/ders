e25e9aacd150020729c4d576ee2cb313ab664e23
betül nar <betnarr@hotmail.com>
2022-09-23 00:05:12 +0300

    1. gün ödevi

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char sınavNotu='C';
+		
+		switch (sınavNotu) {
+		case 'A': 
+			System.out.println("AA geçtin");
+			break;
+		case 'B': 
+			System.out.println("BB geçtin"); break;
+		case 'C': 
+			System.out.println("CC geçtin");break;
+		case 'F': 
+			System.out.println("KALDIN");break;
+			
+			
+			
+		default:
+			System.out.println("YANLIŞ NOT GİRDİNİZ");
+			
+	
+		}
+		
+		
+	}
+
+}
