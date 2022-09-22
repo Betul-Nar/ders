8a63ac0be10b9a67f89c82192079bfcd332fbcab
betül nar <betnarr@hotmail.com>
2022-09-23 00:22:20 +0300

    düzenli

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,27 @@
-import javax.swing.event.SwingPropertyChangeSupport;
-
 public class Main {
 
 	public static void main(String[] args) {
-		char sınavNotu='C';
-		
+		char sınavNotu = 'C';
+
 		switch (sınavNotu) {
-		case 'A': 
+		case 'A':
 			System.out.println("AA geçtin");
 			break;
-		case 'B': 
-			System.out.println("BB geçtin"); break;
-		case 'C': 
-			System.out.println("CC geçtin");break;
-		case 'F': 
-			System.out.println("KALDIN");break;
-			
-			
-			
+		case 'B':
+			System.out.println("BB geçtin");
+			break;
+		case 'C':
+			System.out.println("CC geçtin");
+			break;
+		case 'F':
+			System.out.println("KALDIN");
+			break;
+
 		default:
-			System.out.println("YANLIŞ NOT GİRDİNİZ");
-			
-	
+			System.out.println("");
+
 		}
-		
-		
+
 	}
 
 }
